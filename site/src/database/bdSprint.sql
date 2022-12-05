604f9567b13640278f6c0518f822a853e2ba064a
BrunoGardrich <bruno.garcia@sptech.school>
2022-12-05 02:53:58 -0300

    Melhorando a Dashboard

--- site/src/database/bdSprint.sql
+++ site/src/database/bdSprint.sql
@@ -48,7 +48,8 @@ CREATE TABLE Leitura(
 	idLeitura INT,
 	temperatura FLOAT,
     umidade FLOAT,
-    dtHora DATETIME,
+    dt DATE,
+    HORA TIME,
     fkSensor int,
     foreign key (fkSensor) references Sensor (idSensor),
     primary key (idLeitura, fkSensor)
@@ -73,9 +74,11 @@ insert into Estufa values
 (1,2,1),
 (2,1,1),
 (3,2,2),
-(4,4,2);
-insert into Estufa values 
-(4,4,3);
+(4,4,2),
+(5,3,3),
+(6,3,3),
+(7,3,3);
+
 
 
 
@@ -84,14 +87,23 @@ insert into sensor values
 (null, 'Ativo', 1),
 (null ,'Ativo', 1),
 (null, 'Manutenção', 1),
-(null, 'Inativo', 2);
+(null, 'Inativo', 2),
+(null, 'Ativo', 3);
 
 insert into leitura values 
 
-(1, '22.50', '60.5', '2022-10-10 13:30:00', 1),
-(2, '24.15', '60.5', '2022-10-10 14:00:00', 2),
-(3, '28.96', '60.5', '2022-10-10 14:30:00', 3),
-(4, '21.13', '60.5', '2022-10-10 15:00:00', 4);
+(1, '22.50', '60.5', '2022-10-10','12:00:00', 1),
+(2, '24.15', '55.0', '2022-10-10','13:00:00', 2),
+(3, '28.96', '57.2', '2022-10-10','14:00:00', 3),
+(4, '21.13', '53.9', '2022-10-10','15:00:00', 4),
+(5, '18.50', '54.5', '2022-10-10','16:00:00', 4),
+(6, '20.13', '53.5', '2022-10-10','17:00:00', 4),
+(7, '22.00', '50.1', '2022-10-10','18:00:00', 4),
+(8, '21.19', '47.5', '2022-10-10','19:00:00', 4),
+(9, '19.13', '45.5', '2022-10-10','20:00:00', 4),
+(10, '21.57', '45.9', '2022-10-10','21:00:00', 4),
+(11, '22.03', '46.6', '2022-10-10','22:00:00', 4),
+(12, '24.18', '48.0', '2022-10-10','23:00:00', 4);
 
 delete from Usuario where idUsuario =2;
 
@@ -106,17 +118,22 @@ join empresa on fkEmpresa = idEmpresa
 join leitura on fkSensor = idleitura;
 
 select Empresa.idEmpresa, Estufa.numeroEstufa, Estufa.idEstufa, Sensor.statusSensor, leitura.temperatura,
-leitura.umidade , leitura.dtHora from Empresa join Estufa on fkEmpresa =idEmpresa 
+leitura.umidade , leitura.Hora from Empresa join Estufa on fkEmpresa =idEmpresa 
 join Sensor on idEstufa = fkEstufa 
 join Leitura on idSensor = fkSensor where idEmpresa = 1;
 
 select Empresa.idEmpresa, Estufa.numeroEstufa, Estufa.idEstufa, Sensor.statusSensor, leitura.temperatura,
-        leitura.umidade , leitura.dtHora 
+        leitura.umidade , leitura.dt, leitura.HORA 
         from Empresa join Estufa on fkEmpresa =idEmpresa 
         join Sensor on idEstufa = fkEstufa 
         join Leitura on idSensor = fkSensor where idEmpresa = 1
-                    order by dtHora  desc limit 12;
+                    order by Hora  desc limit 12;
 
+truncate table empresa;
+truncate table usuario;
+truncate table sensor;
+truncate table estufa;
+truncate table leitura;
 
 DROP TABLE usuario;
 DROP TABLE Empresa;
--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -5,19 +5,17 @@ function buscarUltimasMedidas(idEmpresa, limite_linhas) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc`;
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select Empresa.idEmpresa, Estufa.numeroEstufa, Estufa.idEstufa, Sensor.statusSensor, leitura.temperatura,
-        leitura.umidade , leitura.dtHora 
+        leitura.umidade , leitura.dt, leitura.HORA 
         from Empresa join Estufa on fkEmpresa =idEmpresa 
-        join Sensor on idEstufa = fkEstufa join Leitura on idSensor = fkSensor where idEmpresa = ${idEmpresa} order by dtHora desc limit ${limite_linhas}`;
+        join Sensor on idEstufa = fkEstufa join Leitura on idSensor = fkSensor where idEmpresa = ${idEmpresa} order by HORA desc limit ${limite_linhas}`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+
+        instrucaoSql = `select Empresa.idEmpresa, Estufa.numeroEstufa, Estufa.idEstufa, Sensor.statusSensor, leitura.temperatura,
+        leitura.umidade , leitura.dt, leitura.HORA 
+        from Empresa join Estufa on fkEmpresa =idEmpresa 
+        join Sensor on idEstufa = fkEstufa join Leitura on idSensor = fkSensor where idEmpresa = ${idEmpresa} order by HORA desc limit ${limite_linhas}`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -32,23 +30,20 @@ function buscarMedidasEmTempoReal(idEmpresa) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
+
+        instrucaoSql = `select Empresa.idEmpresa, Estufa.numeroEstufa, Estufa.idEstufa, Sensor.statusSensor, leitura.temperatura,
+        leitura.umidade , leitura.dt, leitura.HORA 
+        from Empresa join Estufa on fkEmpresa =idEmpresa 
+        join Sensor on idEstufa = fkEstufa join Leitura on idSensor = fkSensor where idEmpresa = ${idEmpresa} order by HORA desc limit ${limite_linhas}`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+
+        instrucaoSql = `select Empresa.idEmpresa, Estufa.numeroEstufa, Estufa.idEstufa, Sensor.statusSensor, leitura.temperatura,
+        leitura.umidade , leitura.dt, leitura.HORA 
+        from Empresa join Estufa on fkEmpresa =idEmpresa 
+        join Sensor on idEstufa = fkEstufa join Leitura on idSensor = fkSensor where idEmpresa = ${idEmpresa} order by HORA desc limit ${limite_linhas}`;
     } else {
+
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
     }
